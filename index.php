efe6e89e32ab4543a3e570a5684eb33259a2d702
Anand Kumar <codebyaanand@gmail.com>
2021-11-26 22:05:30 +0530

    First page of online quiz

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Online Quiz</title>
+</head>
+<body>
+    <form action="./pages/quests.php">
+        <h1>Online Quiz</h1>
+        <button type="submit">CONTINUE</button>
+    </form>
+</body>
+</html>
