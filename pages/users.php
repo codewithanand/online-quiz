80b1250e7b0d80357d725e436039bfdf93e31202
Anand Kumar <codebyaanand@gmail.com>
2021-11-29 18:35:09 +0530

    Registered users feature added successfully

--- pages/users.php
+++ pages/users.php
@@ -3,6 +3,30 @@
 
     session_start();
     $userId = $_SESSION['userId'];
+
+    $getUserId='';
+    $userName='';
+    $userEmail='';
+    $userTotalScore='';
+    $userGlobalRank='';
+
+    if($_SERVER['REQUEST_METHOD'] == 'POST'){
+        $getUserId = $_POST['user_opt'];
+
+        $sql = "SELECT * FROM user WHERE user_id='$getUserId'";
+        $result = mysqli_query($conn, $sql);
+        $row = mysqli_fetch_assoc($result);
+
+        $userName = $row['user_fname'].' '.$row['user_lname'];
+        $userEmail = $row['email'];
+
+        $sql = "SELECT * FROM global_scoreboard WHERE user_id='$getUserId'";
+        $result = mysqli_query($conn, $sql);
+        $row = mysqli_fetch_assoc($result);
+
+        $userTotalScore = $row['total_score'];
+        $userGlobalRank = $row['user_rank'];
+    }
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -13,48 +37,48 @@
     <title>Registered Users | Online Quiz</title>
 </head>
 <body>
-    <div>
+    <form method="post" action="./users.php">
         <?php
+            // $sql = "SELECT * FROM user WHERE is_admin='no'";
             $sql = "SELECT * FROM user";
             $result = mysqli_query($conn, $sql);
             while($row = mysqli_fetch_assoc($result)){
                 echo '
-                    <div><button>'.$row['user_id'].'</button></div>
+                    <div><input type="radio" name="user_opt" value="'.$row['user_id'].'"><label for="user_opt">'.$row['user_id'].'</label></div>
                 ';
             }
         ?>
-    </div>
+        <button type="submit">SEARCH</button>
+    </form>
     <div>
         <div>
-            <h3>USER NAME</h3>
-            <h3>username@domain.com</h3>
+            <h3><?php echo $userName ?></h3>
+            <h3><?php echo $userEmail ?></h3>
         </div>
         <div>
             <div>
                 <div>
-                    <span>00</span>
+                    <span><?php echo $userGlobalRank ?></span>
                     <span>RANK</span>
-                    <span>123</span>
+                    <span><?php echo $userTotalScore ?></span>
                 </div>
             </div>
             <div>
                 <h2>RECENT QUIZES</h2>
                 <div>
-                    <div>
-                        <span>HTML</span>
-                        <span>11/20</span>
-                    </div>
-                    <div>
-                        <span>HTML</span>
-                        <span>11/20</span>
-                    </div>
-                    <div>
-                        <span>HTML</span>
-                        <span>11/20</span>
-                    </div>
+                    <?php
+                        $sql = "SELECT * FROM user_recent_quizes WHERE user_id='$getUserId'";
+                        $result = mysqli_query($conn, $sql);
+                        while ($row = mysqli_fetch_assoc($result)){
+                            echo '
+                            <div><span>'.$row['quiz_id'].'</span><span>'.$row['quiz_score'].'/'.$row['total_ques'].'</span></div>
+                            ';
+                        }
+                    ?>
                 </div>
             </div>
         </div>
     </div>
+    
 </body>
 </html>
